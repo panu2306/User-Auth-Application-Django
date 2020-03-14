a97202ab16977c17ff851a598a99abb0f791cb2d
panu2306 <pranavbhendawade@gmail.com>
2020-03-14 15:45:44 +0530

    User Authentication Learning Application

---
+++ basic_users/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class BasicUsersConfig(AppConfig):
+    name = 'basic_users'
---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/panu/Documents/profiles/work/django-projects/learning_users/env/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
