af02025f6e29e4424bdd8225e0d3aafa49e0da43
panu2306 <pranavbhendawade@gmail.com>
2020-03-14 15:47:00 +0530

    User model extension for adding profile url & picture

--- basic_users/models.py
+++ basic_users/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfileInfo(models.Model):
+
+    # create relationship with User Model(Don't inherit User Model)
+    user = models.OneToOneField(User)
+
+    # Add additional attributes you want 
+    portfolio = models.URLField(blank=True)
+    picture = models.ImageField(upload_to='profile_pics')
+
+    def __str__(self):
+        return self.user.username
+    
